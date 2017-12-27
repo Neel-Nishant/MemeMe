a2c5f635e94c7ab74ecc831f918a0b98aa10be81
Neel-Nishant <neeljvm@gmail.com>
2017-12-27 16:39:35 +0530

    added files

---
+++ MemeMe/SentMemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SentMemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Neel Nishant on 27/12/17.
+//  Copyright © 2017 Neel Nishant. All rights reserved.
+//
+
+import UIKit
+
+class SentMemeCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Memev1.0/Meme.swift
@@ -0,0 +1,9 @@
+//
+//  Meme.swift
+//  Memev1.0
+//
+//  Created by Neel Nishant on 26/12/17.
+//  Copyright © 2017 Neel Nishant. All rights reserved.
+//
+
+import Foundation
